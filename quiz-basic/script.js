3e98bb3f55c1bcafb2036074fe209dbd2bf6c21f
om dixit <omdixit0902@gmail.com>
2026-01-20 00:04:23 +0530

    Create script.js

---
+++ quiz-basic/script.js
@@ -0,0 +1,9 @@
+const correctAnswer = "8";
+
+function checkAnswer(selected) {
+    if (selected === correctAnswer) {
+        alert("Correct Answer!");
+    } else {
+        alert("Wrong Answer!");
+    }
+}
